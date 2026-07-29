76f17eb6b00a41bbaa715666dc02ba5d0b08aec1
Gabriel Jose Villeda Giron <gabrielvilleda204@gmail.com>
2026-07-29 10:20:55 -0700

    Testing the waitlist page

--- glowpass-waitlist/server/src/server.js
+++ glowpass-waitlist/server/src/server.js
@@ -8,15 +8,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());          // Allows React app to talk to this backend
-app.use(express.json());  // Allows backend to read JSON data sent in request bodies
+app.use(cors());
+app.use(express.json());
 
-//"test" route to make sure the server is alive
+
 app.get('/api/test', (req, res) => {
   res.json({ message: "Hello from the backend server!" });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
